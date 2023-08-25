e4fdf5c396c21dfc21a375e4e94a2838aee7250f
Gustavo Pacheco <ryctabo@gmail.com>
2023-08-25 05:22:59 -0500

    add docker compose and script to init db

--- Dockerfile
+++ Dockerfile
@@ -2,4 +2,5 @@ FROM node:18-alpine
 WORKDIR /app
 COPY . ./
 RUN npm install && npm run build
+EXPOSE 8080
 ENTRYPOINT [ "node", "dist/index.js" ]
---
+++ scripts/mongo-init.js
@@ -0,0 +1,7 @@
+db.getCollection('users').insertOne({
+  displayName: 'Gustavo Pacheco',
+  username: 'ryctabo',
+  email: 'ryctabo@gmail.com',
+  password: 'secretpwd',
+  favorites: []
+})
